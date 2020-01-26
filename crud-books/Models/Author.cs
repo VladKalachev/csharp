214ce6e0533377c591aa221692e0ef97aca8c615
Corvax <vlvete1990@yandex.ru>
2020-01-26 22:03:44 +0300

    Data Annotation

--- crud-books/Models/Author.cs
+++ crud-books/Models/Author.cs
@@ -1,11 +1,20 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BookApiProject.Models
 {
     public class Author
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(100, ErrorMessage = "Firs Name cannot be more than 100 characters")]
         public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100, ErrorMessage = "Last Name cannot be more than 100 characters")]    
         public string LastName { get; set; }
         public virtual Country Country { get; set; }
         public virtual ICollection<BookAuthor> BookAuthors { get; set; }
