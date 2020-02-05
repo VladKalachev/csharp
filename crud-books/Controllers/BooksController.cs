382d1071f98829c454dcbab6be1d1e89e38e65f8
VladKalachev <vlvete1990@yandex.ru>
2020-02-05 18:03:50 +0300

    adding all get actions

--- crud-books/Controllers/BooksController.cs
+++ crud-books/Controllers/BooksController.cs
@@ -14,10 +14,17 @@ namespace BookApiProject.Controllers
     public class BooksController: Controller
     {
         private IBookRepository _bookRepository;
+        private IAuthorRepository _authorRepository;
+        private ICategoryRepository _categoryRepository;
+        private IReviewRepository _reviewRepository;
 
-        public BooksController(IBookRepository bookRepository)
+        public BooksController(IBookRepository bookRepository, IAuthorRepository authorRepository, 
+            ICategoryRepository categoryRepository, IReviewRepository reviewRepository )
         {
             _bookRepository = bookRepository;
+            _authorRepository = authorRepository;
+            _categoryRepository = categoryRepository;
+            _reviewRepository = reviewRepository;
         }
 
         //api/books
@@ -46,5 +53,76 @@ namespace BookApiProject.Controllers
 
             return Ok(booksDto);    
         }
+
+         //api/books/bookId
+        [HttpGet("{bookId}", Name = "GetBook")]
+        [ProducesResponseType(200, Type = typeof(BookDto))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult GetBook(int bookId)
+        {
+            if (!_bookRepository.BookExists(bookId))
+                return NotFound();
+
+            var book = _bookRepository.GetBook(bookId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var bookDto = new BookDto()
+            {
+                Id = book.Id,
+                Title = book.Title,
+                Isbn = book.Isbn,
+                DatePublished = book.DatePublished
+            };
+
+            return Ok(bookDto);
+        }
+
+        //api/books/isbn/bookIsbn
+        [HttpGet("ISBN/{bookIsbn}")]
+        [ProducesResponseType(200, Type = typeof(BookDto))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult GetBook(string bookIsbn)
+        {
+            if (!_bookRepository.BookExists(bookIsbn))
+                return NotFound();
+
+            var book = _bookRepository.GetBook(bookIsbn);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var bookDto = new BookDto()
+            {
+                Id = book.Id,
+                Title = book.Title,
+                Isbn = book.Isbn,
+                DatePublished = book.DatePublished
+            };
+
+            return Ok(bookDto);
+        }
+
+        //api/books/bookId/rating
+        [HttpGet("{bookId}/rating")]
+        [ProducesResponseType(200, Type = typeof(decimal))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult GetBookRating(int bookId)
+        {
+            if (!_bookRepository.BookExists(bookId))
+                return NotFound();
+
+            var rating = _bookRepository.GetBookRating(bookId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(rating);
+        }
+
     }
 }
--- crud-books/Controllers/ReviewsController.cs
+++ crud-books/Controllers/ReviewsController.cs
@@ -19,25 +19,25 @@ namespace BookApiProject.Controllers
 
         public ReviewsController(IReviewerRepository reviewerRepository, IReviewRepository reviewRepository, IBookRepository bookRepository)
         {
-            reviewerRepository = _reviewerRepository;
-            reviewRepository = _reviewRepository;
-            bookRepository = _bookRepository;
+            _reviewerRepository = reviewerRepository;
+            _reviewRepository = reviewRepository;
+            _bookRepository = bookRepository;
         }
 
         //api/reviews
         [HttpGet]
         [ProducesResponseType(200, Type = typeof(IEnumerable<ReviewDto>))]
         [ProducesResponseType(400)]
-        public ActionResult GetReviews()
+        public IActionResult GetReviews()
         {
             var reviews = _reviewRepository.GetReviews();
 
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
-            
+
             var reviewsDto = new List<ReviewDto>();
 
-            foreach (var review in reviewsDto)
+            foreach (var review in reviews)
             {
                 reviewsDto.Add(new ReviewDto
                 {
@@ -48,7 +48,89 @@ namespace BookApiProject.Controllers
                 });
             }
 
-            return Ok(reviewsDto);    
+            return Ok(reviewsDto);
+        }
+
+        //api/reviews/reviewId
+        [HttpGet("{reviewId}",Name = "GetReview")]
+        [ProducesResponseType(200, Type = typeof(ReviewDto))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult GetReview(int reviewId)
+        {
+            if (!_reviewRepository.ReviewExists(reviewId))
+                return NotFound();
+
+            var review = _reviewRepository.GetReview(reviewId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var reviewDto = new ReviewDto()
+            {
+                Id = review.Id,
+                Headline = review.Headline,
+                Rating = review.Rating,
+                ReviewText = review.ReviewText
+            };
+
+            return Ok(reviewDto);
+        }
+        
+        //api/reviews/books/bookId
+        [HttpGet("books/{bookId}")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<ReviewDto>))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult GetReviewsForABook(int bookId)
+        {
+            if (!_bookRepository.BookExists(bookId))
+                return NotFound();
+            
+            var reviews = _reviewRepository.GetReviewsOfABook(bookId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var reviewsDto = new List<ReviewDto>();
+            foreach (var review in reviews)
+            {
+                reviewsDto.Add(new ReviewDto()
+                {
+                    Id = review.Id,
+                    Headline = review.Headline,
+                    Rating = review.Rating,
+                    ReviewText = review.ReviewText
+                });
+            }
+
+            return Ok(reviewsDto);
+        }
+
+        //api/reviews/reviewId/book
+        [HttpGet("{reviewId}/book")]
+        [ProducesResponseType(200, Type = typeof(BookDto))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult GetBookOfAReview(int reviewId)
+        {
+            if (!_reviewRepository.ReviewExists(reviewId))
+                return NotFound();
+
+            var book = _reviewRepository.GetBookOfAReview(reviewId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var bookDto = new BookDto()
+            {
+                Id = book.Id,
+                Title = book.Title,
+                Isbn = book.Isbn,
+                DatePublished = book.DatePublished
+            };
+
+            return Ok(bookDto);
         }
     }
 }
